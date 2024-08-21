412d74d763e3e69f3bc4cf8e153ef56a713f35d2
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2024-08-21 11:21:26 +0530

    Lab_1_IoP

---
+++ C/Lab1_IoP.c
@@ -0,0 +1,22 @@
+#include<stdio.h> // --> Header File
+
+int main() // --> main() function
+{
+    printf("Welcome to AI Department\n");
+    printf("Hello world\n\n");
+
+    // Sum of two numbers
+    int a, b, sum1;
+    a = 2;
+    b = 3;
+    sum1 = a+b;
+    printf("Sum of a and b is: %d\n\n", sum1);
+
+    // Using scanf() function
+    int c, d, sum2;
+    scanf("%d%d", &c, &d);
+    sum2 = c+d;
+    printf("Sum of c and d is: %d\n", sum2);
+
+    return 0;
+}
