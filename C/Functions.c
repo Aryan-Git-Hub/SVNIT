10a10897cee34a70aa5a9d524d8e45b946a3eba7
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2024-10-08 22:14:33 +0530

    Functions.c

---
+++ C/Functions.c
@@ -0,0 +1,50 @@
+// #include<stdio.h>
+// #define add_six(x) x+6 // ---> This will return same expression as given i.e. it give x+6 if we put bracket then it will give us with bracket.
+
+// int main() {
+//     int i = 5;
+//     int j = add_six(5)*add_six(5); // ---> this will give us expression like: 5+6*5+6 = 41
+//     printf("%d%d", i, j);
+
+//     return 0;
+// }
+
+// "&&" have more precision than "||"
+
+// ***********************************************************************************************************
+// #include <stdio.h>
+
+// int main() {
+//     int i = 5; // Initial value of i
+
+//     // Bitwise left shift
+//     int left_shifted = i << 2; // ---> i*(2^2)
+//     printf("Left shifted: %d\n", left_shifted);
+
+//     // Bitwise right shift
+//     int right_shifted = i >> 2; // ---> i/(2^2)
+//     printf("Right shifted: %d\n", right_shifted);
+
+//     return 0;
+// }
+
+
+
+// ***********************************************************************************************************
+// #include<stdio.h>
+
+// float f(n)
+// {
+//     return (2*n);
+// }
+
+// int main() {
+//     float x = 2.3;
+//     printf("%f", f(x));
+
+//     return 0;
+// }
+
+
+
+// ***********************************************************************************************************
