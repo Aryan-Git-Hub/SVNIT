b7beeae57578a5769d8ba1fda0e7986ac309ec88
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2024-10-17 16:08:51 +0530

    changing code of P53.c

--- C/Assignments/Lab_5_and_6/P53.c
+++ C/Assignments/Lab_5_and_6/P53.c
@@ -5,38 +5,36 @@ int main() {
     printf("Enter the value of n: ");
     scanf("%d", &n);
 
-    int a = 0;
-    int b = 0;
-    for (int i = 1; i <= 2*n-1; i++)
+
+    for (int i = 0; i < n; i++)
     {
-        a++;
-        for (int j = 1; j <= n+a-1; j++)
+        for (int j = 0; j < n-i-1; j++)
         {
-            if (i<=n)
-            {
-                if (j<n-a+1)
-                {
-                    printf(" ");
-                } else
-                {
-                    printf("*");
-                }
-            } else
-            {
-                if (j<=i-n || j>=2*n-1-b)
-                {
-                    printf(" ");
-                } else
-                {
-                    printf("*");
-                }
-            }
+            printf(" ");
         }
-        if (i>n) b++;
+
+        for (int j = 0; j < 2*i+1; j++)
+        {
+            printf("*");
+        }
         
         printf("\n");
     }
-    
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n-1; j >= n-i-1; j--)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j < 2*(n-i-1)-1; j++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
 
     return 0;
 }
