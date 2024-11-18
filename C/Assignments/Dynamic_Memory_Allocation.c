b970c5bdbc982f52d4680bf9ca388f3b59023bce
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2024-11-18 23:58:50 +0530

    Pointers

---
+++ C/Assignments/Dynamic_Memory_Allocation.c
@@ -0,0 +1,6 @@
+// int* ptr_arr = (int*)malloc(n * sizeof(int));
+// 1) malloc(total_bytes)
+// 2) calloc(n, elem_size)
+// 3) realloc(ptr_arr, new_size) ---> to change the size of an array
+// 4) free(ptr_arr) ---> release dynamic memory of an array
+
--- C/Assignments/practice.c
+++ C/Assignments/practice.c
@@ -1,17 +1,47 @@
-#include<stdio.h>
-#include<string.h>
+// #include<stdio.h>
+// #include<string.h>
 
-int main() {
-    char arr[50];
-    printf("Enter any string: ");
-    fgets(arr, 50, stdin);
-    char* p = arr;
+// int main() {
+//     char arr[50];
+//     printf("Enter any string: ");
+//     fgets(arr, 50, stdin);
+//     char* p = arr;
 
-    for (int i = 0; i < strlen(arr); i+=2)
-    {
-        printf("%c", *(p+i));
-    }
+//     for (int i = 0; i < strlen(arr); i+=2)
+//     {
+//         printf("%c", *(p+i));
+//     }
     
+
+//     return 0;
+// }
+
+#include<stdio.h>
+int* alter_arr(int n, int arr[]) {
+    int temp[(n+1)/2];
+    int j = 0;
+    for (int i = 0; i < n; i+=2)
+    {
+        temp[j] = arr[i];
+        j++;
+    }
+    int* ptr = temp;
+    return ptr;
+    
+}
+int main() {
+    // initializing an array
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    int* new_arr = alter_arr(n, arr);
 
     return 0;
 }
