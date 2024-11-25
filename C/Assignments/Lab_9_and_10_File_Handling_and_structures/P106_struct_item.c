ff248f988547527703fab7d6e75231f64ab2169f
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2024-11-26 02:01:08 +0530

    P106 struct item using pointers

--- C/Assignments/Lab_9_and_10_File_Handling_and_structures/P106_struct_item.c
+++ C/Assignments/Lab_9_and_10_File_Handling_and_structures/P106_struct_item.c
@@ -1,16 +1,33 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 struct item
 {
     char item_name[30];
     int quantity;
     int price;
+    int amount;
 };
 
+void read_item(struct item* i) {
+    printf("Enter name of item: ");
+    scanf("%s", i->item_name);
+    printf("Enter quantitiy of this item: ");
+    scanf("%d", &i->quantity);
+    printf("Enter price of this item: ");
+    scanf("%d", &i->price);
+    i->amount = i->quantity*i->price;
+
+    printf("\nYour item details are:\n");
+    printf("\tItem name: %s\n", i->item_name);
+    printf("\tItem quantity: %d\n", i->quantity);
+    printf("\tItem price: %d\n", i->price);
+    printf("\tItem total amount: %d\n", i->amount);
+}
 
 int main() {
-    struct item i1 = {"item 1", 5, 200};
-    printf("%d", i1.price);
+    struct item* i1 = (struct item*)malloc(sizeof(struct item*));
+    read_item(i1);
 
     return 0;
 }
